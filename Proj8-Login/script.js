ea389754180dbbab949793d74bd8956a82a9ca27
skshm <eminate894567@protonmail.com>
2023-08-21 21:56:56 +0530

    wavy form

---
+++ Proj8-Login/script.js
@@ -0,0 +1,8 @@
+const labels = document.querySelectorAll('.form-control label')
+
+labels.forEach(label => {
+    label.innerHTML = label.innerText // split each letter into an array
+        .split('')
+        .map((letter, idx) => `<span style="transition-delay:${idx * 40}ms">${letter}</span>`) // wrap each letter in a span
+        .join('') // join the array back into a string
+})
