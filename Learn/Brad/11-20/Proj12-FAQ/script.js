23a11b763647b5def21b1b5c033d85ffd52f3a54
skshm <eminate894567@protonmail.com>
2024-02-24 21:18:51 +0530

    Branch 4 deploy

--- Learn/Brad/11-20/Proj12-FAQ/script.js
+++
@@ -1,9 +0,0 @@
-const toggles = document.querySelectorAll(".faq-toggle");
-
-toggles.forEach((toggle) => {
-  toggle.addEventListener("click", () => {
-    toggle.parentNode.classList.toggle("active");
-    // ParentNode is the parent of the current node
-    // classList is the list of classes of the current node
-  });
-});
